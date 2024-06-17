37efbcebb9810e70b70748074b9ecea04c4fc46c
jicadz <cadiz.joshuaiangit2011@gmail.com>
2024-06-17 18:20:43 +0800

    8th commit
    testimonials, join us page, footer edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ module.exports = {
         'button-shadow2' : '0 0 8px white'
       },
       backgroundImage: {
-        'about-image' : "linear-gradient(rgba(0,0,0,0.75),rgba(0,0,0,0.75)),url('./assets/image2.jpg')"
+        'about-image' : "linear-gradient(rgba(0,0,0,0.75),rgba(0,0,0,0.75)),url('./assets/image2.jpg')",
+        'user-1' : "url('./assets/user1.jpg')",
+        'user-2' : "url('./assets/user2.jpg')",
+        'user-3' : "url('./assets/user3.jpg')"
       }
     },
   },
